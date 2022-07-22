6efef7e61369b9198ed1d97263b53af0f7ffdf41
TC-Avin <106251495+TC-Avin@users.noreply.github.com>
2022-07-23 00:08:21 +0530

    latest

--- src/components/PopModel.js
+++ src/components/PopModel.js
@@ -1,24 +1,31 @@
-import React, { useEffect, useState } from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import React, { useEffect, useState } from "react";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 
 const PopModel = (props) => {
-  const {
-    className,data
-  } = props;
+  const { className, data } = props;
 
   const [modal, setModal] = useState(false);
-  const toggle = () =>{ setModal(!modal);props.close(false)}
+  const toggle = () => {
+    setModal(!modal);
+    props.close(false);
+  };
 
-  useEffect(()=>{
-    setModal(props.open)
-  },[props.open])
+  useEffect(() => {
+    setModal(props.open);
+  }, [props.open]);
 
-  const closeBtn = <button className="close" onClick={toggle}>&times;</button>;
+  const closeBtn = (
+    <button className="close" onClick={toggle}>
+      &times;
+    </button>
+  );
 
   return (
     <div>
       <Modal isOpen={modal} toggle={toggle} className={className}>
-        <ModalHeader toggle={toggle} close={closeBtn}>Modal title</ModalHeader>
+        <ModalHeader toggle={toggle} close={closeBtn}>
+          Modal title
+        </ModalHeader>
         <ModalBody>
           {/* Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
           dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
@@ -26,25 +33,34 @@ const PopModel = (props) => {
           fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
           mollit anim id est laborum. */}
 
-          {
-
-           Object.keys(props.   data).map((val)=>{
-            if(props.selected.includes(val)){
-                return (<div>
-                  <b>  {val}</b>:&nbsp;{data[val]}
-                </div>)
+          {Object.keys(props.data).map((val) => {
+            if (props.selected.includes(val)) {
+              if (val == "checkbox") {
+                return (
+                  <div>
+                    <b> {val}</b>:&nbsp;{
+                        Object.keys(props.data.checkbox).map((d)=>{if(props.data.checkbox[d]){return (<span>{d},</span>)}})
+                    }
+                  </div>
+                );
+              } else {
+                return (
+                  <div>
+                    <b> {val}</b>:&nbsp;{data[val]}
+                  </div>
+                );
+              }
             }
-           })
-
-
-          }
+          })}
         </ModalBody>
         <ModalFooter>
-          <Button color="secondary" onClick={toggle}>Cancel</Button>
+          <Button color="secondary" onClick={toggle}>
+            Cancel
+          </Button>
         </ModalFooter>
       </Modal>
     </div>
   );
-}
+};
 
 export default PopModel;
